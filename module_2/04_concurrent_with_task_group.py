68dea558cd899749e178e97d190d4be4f3c3302e
Muhammad Atif <atifazad83@gmail.com>
2025-05-28 14:00:50 +0200

    added code to demo concurren task management with TaskGroup

---
+++ module_2/04_concurrent_with_task_group.py
@@ -0,0 +1,65 @@
+# This script demonstrates how to manage multiple concurrent asynchronous tasks using asyncio.TaskGroup.
+# TaskGroup was introduced in Python 3.11.
+
+import asyncio
+import time
+
+
+async def coro_1():
+    print("Coro 1 started.")
+    await asyncio.sleep(5)
+    print("Coro 1 completed.")
+
+    return "Coro 1 result"
+
+
+async def coro_2():
+    print("Coro 2 started.")
+    await asyncio.sleep(1)
+    print("Coro 2 completed.")
+
+    return "Coro 2 result"
+
+
+async def coro_3():
+    print("Coro 3 started.")
+    await asyncio.sleep(3)
+    print("Coro 3 completed.")
+
+    return "Coro 3 result"
+
+
+async def main():
+    print("Running tasks concurrently with TaskGroup:")
+
+    start_time = time.perf_counter()
+    tasks = []
+    async with asyncio.TaskGroup() as tg:
+        tasks.append(tg.create_task(coro_1()))
+        tasks.append(tg.create_task(coro_2()))
+        tasks.append(tg.create_task(coro_3()))
+    end_time = time.perf_counter()
+
+    for task in tasks:
+        result = await task
+        print(f"{result}")
+
+    print(f"All tasks completed in {end_time - start_time:.2f} seconds")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+# OUTPUT:
+# Running tasks concurrently with TaskGroup:
+# Coro 1 started.
+# Coro 2 started.
+# Coro 3 started.
+# Coro 2 completed.
+# Coro 3 completed.
+# Coro 1 completed.
+# Coro 1 result
+# Coro 2 result
+# Coro 3 result
+# All tasks completed in 5.00 seconds
