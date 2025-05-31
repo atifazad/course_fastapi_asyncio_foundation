dc6f15791d3e8b3900522ad806b020d11e8ba1d7
Muhammad Atif <atifazad83@gmail.com>
2025-05-31 14:46:29 +0200

    updated the asyncio.as_completed demo

--- module_2/05_as_completed.py
+++ module_2/05_as_completed.py
@@ -1,30 +1,35 @@
-# Thi script demonstrates the use of `asyncio.as_completed()`
-# to run multiple asynchronous tasks concurrently and process their results as they complete.
-# This is useful when you want to handle results in the order of completion rather than the order of submission.
+# Consider that we have to scrape data from three websites.
+# Data from three websites can be scraped independent of each other.
+# Additionally, we want to process the data as soon as
+# data is retrieved from one of the website without waiting for the rest of the batch to complete.
+# This is a usecase for asyncio.as_completed()
 
 import asyncio
 import time
 
 
-async def coro_1():
-    print("Coro 1 started.")
-    await asyncio.sleep(5)
-    print("Coro 1 completed.")
-    return "Coro 1 result"
+async def scrape_data_from_website_1():
+    print(f"Starting to scrape website 1")
+    await asyncio.sleep(.9)
+    print(f"Done scraping website 1")
+
+    return "website_1_data"
 
 
-async def coro_2():
-    print("Coro 2 started.")
-    await asyncio.sleep(1)
-    print("Coro 2 completed.")
-    return "Coro 2 result"
+async def scrape_data_from_website_2():
+    print(f"Starting to scrape website 2")
+    await asyncio.sleep(.5)
+    print(f"Done scraping website 2")
+
+    return "website_2_data"
 
 
-async def coro_3():
-    print("Coro 3 started.")
-    await asyncio.sleep(3)
-    print("Coro 3 completed.")
-    return "Coro 3 result"
+async def scrape_data_from_website_3():
+    print(f"Starting to scrape website 3")
+    await asyncio.sleep(.6)
+    print(f"Done scraping website 3")
+
+    return "website_3_data"
 
 
 async def main():
@@ -33,18 +38,19 @@ async def main():
     start_time = time.perf_counter()
 
     tasks = [
-        asyncio.create_task(coro_1()),
-        asyncio.create_task(coro_2()),
-        asyncio.create_task(coro_3()),
+        asyncio.create_task(scrape_data_from_website_1()),
+        asyncio.create_task(scrape_data_from_website_2()),
+        asyncio.create_task(scrape_data_from_website_3()),
     ]
 
+    print("\nResults:")
     for completed_task in asyncio.as_completed(tasks):
         result = await completed_task
-        print(f"Task completed with result: {result}")
+        print(f"{result}")
 
     end_time = time.perf_counter()
 
-    print(f"All tasks completed in {end_time - start_time:.2f} seconds")
+    print(f"Total time: {end_time - start_time:.2f} seconds")
 
 
 if __name__ == "__main__":
