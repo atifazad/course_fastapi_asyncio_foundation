233b4c14a0ca7a108d4eb2633fb9d588ce4d57d6
Muhammad Atif <atifazad83@gmail.com>
2025-05-31 14:42:40 +0200

    replaced sequential tasks demo with a real world use case

--- module_2/01_sequential_with_await.py
+++ module_2/01_sequential_with_await.py
@@ -1,45 +1,54 @@
-# This script demonstrates how to run multiple asynchronous tasks sequentially using `await`.
-# Compare this with the concurrent execution in `02_concurrent_with_tasks.py`and 03_concurrent_with_gather.py
+# Some of the operations are naturally sequential. Take this example...
+#
+# 1. Retrieve the product data from an API
+# 2. Validate the data
+# 3. Save the data into database
+#
+# We must execute these steps in order as every next step depends on the previous one.
+# Code below demostrates how to perform such operations with asyncio.
+
 
 import asyncio
 import time
 
 
-async def coro_1():
-    print("Coro 1 started.")
-    await asyncio.sleep(5)
-    print("Coro 1 completed.")
+async def get_data(url):
+    print("get_data started")
+    await asyncio.sleep(.5)
+    print("get_data completed")
 
-    return "Coro 1 result"
-
-
-async def coro_2():
-    print("Coro 2 started.")
-    await asyncio.sleep(1)
-    print("Coro 2 completed.")
-
-    return "Coro 2 result"
+    return "response_data_from_url"
 
 
-async def coro_3():
-    print("Coro 3 started.")
-    await asyncio.sleep(3)
-    print("Coro 3 completed.")
+async def validate(data):
+    print("validate started.")
+    await asyncio.sleep(.2)
+    print("validate completed.")
 
-    return "Coro 3 result"
+    return data     # validated_data
+
+
+async def save_in_db(validated_data):
+    print("save_in_db started.")
+    await asyncio.sleep(.3)
+    print("save_in_db completed.")
+
+    return True
 
 
 async def main():
-    print("Running tasks sequentially with await:")
+    print("Retrieving, validating and saving product data sequentially...")
 
     start_time = time.perf_counter()
-    r1 = await coro_1()
-    r2 = await coro_2()
-    r3 = await coro_3()
+
+    # Sequentially retrieve, validate and save data.
+    data = await get_data(url="http:api.example.com/product/1")
+    validated_data = await validate(data)
+    await save_in_db(validated_data)
+
     end_time = time.perf_counter()
 
-    print(f"Results: \n {r1}\n {r2}\n {r3}")
-    print(f"All tasks completed in {end_time - start_time:.2f} seconds")
+    print(f"Product saved in {end_time - start_time:.2f} seconds")
 
 
 if __name__ == "__main__":
@@ -47,14 +56,14 @@ if __name__ == "__main__":
 
 
 # OUTPUT:
-# Running tasks sequentially with await:
-# Task 1 started.
-# Task 1 completed.
-# Task 2 started.
-# Task 2 completed.
-# Task 3 started.
-# Task 3 completed.
-# All tasks completed in 9.00 seconds
+# Retrieving, validating and saving product data sequentially...
+# get_data started
+# get_data completed
+# validate started.
+# validate completed.
+# save_in_db started.
+# save_in_db completed.
+# Product saved in 1.00 seconds
 
 
 # NOTE: Due to the sequential execution of async tasks,
